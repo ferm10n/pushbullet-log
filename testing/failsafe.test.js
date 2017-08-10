584d2af762d4d52b5d4274a34b8f233fe1cec582
John Sanders <jws121295@gmail.com>
2017-08-10 17:52:40 -0400

    connection checking

--- testing/failsafe.test.js
+++ testing/failsafe.test.js
@@ -2,7 +2,7 @@ import test from 'ava'
 const mockRequest = require('./utils.js').mockRequest
 const PL = require('../index')
 
-test('pushbullet error', t => {
+test.serial('pushbullet error', t => {
   PL._dns = { lookup: (domain, cb) => { cb() } }
   PL._request = mockRequest()
   const p = new PL({
@@ -17,5 +17,22 @@ test('pushbullet error', t => {
   setTimeout(() => {
     PL._request.listeners.error(new Error('test error'))
   }, 10)
+  return logPromise
+})
+
+test.serial('connection error', t => {
+  PL._request = mockRequest()
+  PL._dns = {
+    lookup: (domain, cb) => { cb(new Error('dns error')) }
+  }
+  const p = new PL({
+    token: 'test',
+    channel: 'test',
+    useConsole: false
+  })
+
+  t.plan(1)
+  p.originalConsole.error = (msg, err) => { t.is(err.message, 'dns error') }
+  const logPromise = p.log('fail log')
   return logPromise
 })
